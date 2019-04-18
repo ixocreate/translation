7a03184ef329f09748212d7f2a6b036eae8f9a7d
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-04-19 01:11:50 +0200

    cleanup

--- src/TranslationConfigurator.php
+++ src/TranslationConfigurator.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 
 namespace Ixocreate\Translation;
 
-use Ixocreate\Application\ConfiguratorInterface;
+use Ixocreate\Application\Configurator\ConfiguratorInterface;
 use Ixocreate\Application\Service\ServiceRegistryInterface;
 use Ixocreate\Translation\Config\Config;
 
