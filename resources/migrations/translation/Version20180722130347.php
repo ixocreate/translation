98f5d8af614ff2f80634c7fb1a8791a41f932976
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2021-01-29 11:28:53 +0100

    update migrations

--- resources/migrations/translation/Version20180722130347.php
+++ resources/migrations/translation/Version20180722130347.php
@@ -10,35 +10,37 @@ declare(strict_types=1);
 namespace Ixocreate\Migration;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\Migrations\AbstractMigration;
 use Ixocreate\Schema\Type\UuidType;
 
 class Version20180722130347 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         $table = $schema->createTable('translation_definition');
         $table->addColumn('id', UuidType::serviceName());
-        $table->addColumn("name", Type::STRING);
-        $table->addColumn("catalogue", Type::STRING);
-        $table->addColumn("files", Type::JSON);
-        $table->addColumn("placeholders", Type::JSON);
-        $table->setPrimaryKey(["id"]);
+        $table->addColumn('name', Types::STRING);
+        $table->addColumn('catalogue', Types::STRING);
+        $table->addColumn('files', Types::JSON);
+        $table->addColumn('placeholders', Types::JSON);
+
+        $table->setPrimaryKey(['id']);
 
         $table = $schema->createTable('translation_translation');
-        $table->addColumn("id", UuidType::serviceName());
-        $table->addColumn("definitionId", UuidType::serviceName());
-        $table->addColumn("locale", Type::STRING);
-        $table->addColumn("message", Type::TEXT)->setNotnull(false);
-        $table->setPrimaryKey(["id"]);
-        $table->addForeignKeyConstraint('translation_definition', ['definitionId'], ['id'], ["onDelete" => "CASCADE"]);
-        $table->addUniqueIndex(["definitionId", "locale"]);
+        $table->addColumn('id', UuidType::serviceName());
+        $table->addColumn('definitionId', UuidType::serviceName());
+        $table->addColumn('locale', Types::STRING);
+        $table->addColumn('message', Types::TEXT)->setNotnull(false);
+
+        $table->setPrimaryKey(['id']);
+        $table->addForeignKeyConstraint('translation_definition', ['definitionId'], ['id'], ['onDelete' => 'CASCADE']);
+        $table->addUniqueIndex(['definitionId', 'locale']);
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
-        $schema->dropTable("translation_definition");
-        $schema->dropTable("translation_translation");
+        $schema->dropTable('translation_definition');
+        $schema->dropTable('translation_translation');
     }
 }
