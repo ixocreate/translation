9c8247113254f0964cb8ad1ead944c6cb28f0188
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2021-02-09 09:03:49 +0100

    remove directory scans from bootstrap

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -10,6 +10,13 @@ declare(strict_types=1);
 namespace Ixocreate\Admin;
 
 use Ixocreate\Application\Http\Middleware\MiddlewareConfigurator;
+use Ixocreate\Translation\Action\CatalogueIndexAction;
+use Ixocreate\Translation\Action\DetailAction;
+use Ixocreate\Translation\Action\IndexAction;
+use Ixocreate\Translation\Action\SaveAction;
 
 /** @var MiddlewareConfigurator $middleware */
-$middleware->addDirectory(__DIR__ . '/../src/Action', true);
+$middleware->addAction(CatalogueIndexAction::class);
+$middleware->addAction(DetailAction::class);
+$middleware->addAction(IndexAction::class);
+$middleware->addAction(SaveAction::class);
